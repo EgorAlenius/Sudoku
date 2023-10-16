83e66185100e8ab575067dbf0bb0304cae48177d
Your Name <you@example.com>
2023-10-16 21:43:57 +0300

    buttons update

--- js/resolving.js
+++ js/resolving.js
@@ -1,6 +1,6 @@
 let table = document.getElementById('table');
 document.getElementById('solveButton').addEventListener("click", solveFromArray);
-// document.getElementById('clearButton').addEventListener("click", cleaning);
+document.getElementById('clearButton').addEventListener("click", cleaning);
 let arr = [[0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0]];
 //let arr = [[1, 0, 7, 0, 0, 0, 5, 2, 0], [0, 0, 0, 0, 4, 5, 0, 0, 0], [0, 0, 8, 3, 0, 0, 1, 0, 6], [8, 0, 0, 9, 0, 0, 0, 0, 0], [0, 0, 0, 6, 0, 7, 0, 0, 0], [6, 0, 0, 0, 0, 0, 9, 8, 0], [0, 3, 0, 0, 0, 0, 0, 0, 0], [0, 5, 0, 0, 0, 0, 4, 9, 0], [0, 0, 1, 2, 0, 4, 0, 0, 0]]
 //let arr = [[0, 1, 0, 2, 0, 0, 3, 0, 0], [2, 0, 0, 5, 3, 0, 0, 0, 9], [0, 5, 0, 4, 0, 0, 0, 0, 0], [0, 6, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 7, 0, 0, 0, 6], [3, 4, 0, 0, 0, 5, 0, 0, 0], [0, 7, 1, 0, 0, 6, 0, 2, 0], [6, 0, 0, 0, 0, 1, 0, 0, 7], [0, 0, 0, 0, 0, 4, 8, 0, 0]]
@@ -382,7 +382,9 @@ function solveFromArray() {
     console.log("Iterations - ", Iter);
 }
 
-// function cleaning() {
+function cleaning() {
+    window.location.reload();
+}
 //     for (let i = 0; i < 9; i++) {
 //         for (let j = 0; j < 9; j++) {
 //             let newInput = document.createElement('input');
